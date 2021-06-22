cad9e3db46cfbee15001cf3b9e3c14006f7d4d20
JessicaJulieth <3497@holbertonschool.com>
2021-06-21 23:42:49 -0500

    The right form to assign a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
+/**
  * main - bring back if n is positive, negative or zero
- * 
- * Return: Always 0 () 
+ *
+ * Return: Always 0 ()
 */
 int main(void)
 {
@@ -13,9 +13,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-        {
+	{
 		printf("%d is positive\n", n);
-        }
+	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
