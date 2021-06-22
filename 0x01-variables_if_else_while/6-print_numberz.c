eb9d557ce637a4e7b3a9b6cb2b3b217fc09b16d1
JessicaJulieth <3497@holbertonschool.com>
2021-06-22 16:13:42 -0500

    Print the number whitout printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print the numbers whit putchar
+ *
+ * Return: Always 0 ()
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
