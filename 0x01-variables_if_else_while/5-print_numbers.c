5b0d724101973ec8977dc0286cc0bce655a6866e
JessicaJulieth <3497@holbertonschool.com>
2021-06-22 12:31:16 -0500

    Print he numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print the numbers
+ *
+ * Return: Always 0 ()
+*/
+int main(void)
+{
+	printf("\n 0123456789");
+	putchar('\n');
+	return (0);
+}
