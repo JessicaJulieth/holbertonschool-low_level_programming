6901695369ea9d6053dfd7338bfe27e88870f58d
JessicaJulieth <3497@holbertonschool.com>
2021-06-23 17:57:58 -0500

    Print whith _putchar Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/*
+ *Print Holberton 
+ *
+ * 
+ *
+ */
+
+int main() 
+{
+    char Name0[]="Holberton\n";
+    int i= 0;       
+    for (i = 0; Name0[i] != '\0'; i++)
+    {
+        _putchar(Name0[i]);
+    }
+    return(0);
+}
