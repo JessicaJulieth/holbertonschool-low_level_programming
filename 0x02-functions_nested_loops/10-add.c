a768c74928428293193c8b703985c8e3a24ebed8
JessicaJulieth <3497@holbertonschool.com>
2021-06-24 21:28:44 -0500

    Add m + p

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - function that adds two integers and returns the result.
+ * @m: variable type int
+ * @p: variable type int
+ * Return: returns the result.
+ */
+int add(int m, int p)
+{
+	int x = 0;
+
+	x = m + p;
+	return (x);
+}
