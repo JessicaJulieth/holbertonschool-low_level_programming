514975829fdacd3d53b3d72d3446d0229094c112
JessicaJulieth <3497@holbertonschool.com>
2021-06-25 02:46:48 -0500

    Write a function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#ifndef Holberton_H
+#define Holberton_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
+
+int _isupper(int c);
+
+#endif
