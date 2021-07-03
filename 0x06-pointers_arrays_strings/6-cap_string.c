d61c8b5393bd4987dd18164fcf6a55c87e1f7680
JessicaJulieth <3497@holbertonschool.com>
2021-07-02 23:34:03 -0500

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Function that capitalizes all words of a string.
+ * @s: Stores the character string that is modified.
+ * Return: We return the start of the string.
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] = s[0] - 32;
+	}
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		if (
+			(
+				s[i] == ' '
+				|| s[i] == '\t'
+				|| s[i] == '\n'
+				|| s[i] == ','
+				|| s[i] == ';'
+				|| s[i] == '.'
+				|| s[i] == '!'
+				|| s[i] == '?'
+				|| s[i] == '"'
+				|| s[i] == '('
+				|| s[i] == ')'
+				|| s[i] == '{'
+				|| s[i] == '}'
+				) && (s[i + 1] > 'a'
+				     && s[i + 1] < 'z')
+			)
+		{
+			s[i + 1] -= 32;
+		}
+	}
+
+	return (s);
+
+}
