f2bf13f595f8b9281da496b94eed71d5ad915779
JessicaJulieth <3497@holbertonschool.com>
2021-07-07 10:38:31 -0500

    Print cheesboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_chessboard - print chessboard
+ * @a: is a streang
+ * Return: none
+ */
+void print_chessboard(char (*a)[8])
+{
+	int c1, c2;
+	for (c1 = 0; c1 < 8; c1++)
+	{
+		for (c2 = 0; c2 < 8; c2++)
+		{
+			_putchar(a[c1][c2]);
+		}
+		_putchar('\n');
+	}
+}
