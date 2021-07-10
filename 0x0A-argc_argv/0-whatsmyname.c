7adaf3ae3b6f54e5658c6cbc15eaa65a7e355732
JessicaJulieth <3497@holbertonschool.com>
2021-07-09 20:12:22 -0500

    Prints my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Print my name.
+ * @argc: Argument Counter
+ * @argv: Argument vector
+ * Return: (0)
+ */
+int main(int argc, char **argv)
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
