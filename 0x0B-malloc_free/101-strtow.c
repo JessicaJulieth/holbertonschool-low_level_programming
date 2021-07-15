0e75cc692bc6dfc30d6f4d1f4e660144252d21b0
JessicaJulieth <3497@holbertonschool.com>
2021-07-14 22:22:12 -0500

    Splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * strtow - splits strings to words
+ * @str: string 
+ * Return: pointer to an array of words
+ */
+
+char **strtow(char *str)
+{
+	char **r;
+	int x, y, c, len, z, a;
+
+	c = z = 0;
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (str[x] != ' ' && (str[x + 1] == ' ' || str[x + 1] == '\0'))
+			c++;
+	}
+	if (c == 0)
+		return (NULL);
+	r = malloc(((c + 1) * sizeof(char *)));
+	if (r == NULL)
+		return (NULL);
+	for (x = 0; str[x] !=  '\0' && z < c; x++)
+	{
+		if (str[x] != ' ')
+		{
+			len = 0;
+			y = x;
+			while (str[y] != ' ' && str[y] != '\0')
+				y++, len++;
+			r[z] = malloc((len + 1) * sizeof(char));
+			if (r[z] == NULL)
+			{
+				for (z = z - 1; z >= 0; z++)
+					free(r[z]);
+				free(r);
+				return (NULL);
+			}
+			for (a = 0; a < len; a++, x++)
+				r[z][a] = str[x];
+			r[z++][a] = '\0';
+		}
+	}
+	r[z] = NULL;
+	return (r);
+}
