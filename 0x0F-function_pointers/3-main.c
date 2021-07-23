6f90d204663989cc13248b410aaa2407109d38f2
JessicaJulieth <3497@holbertonschool.com>
2021-07-22 20:18:59 -0500

    Performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - Entry point to perform operation
+ * @ac: argument counter
+ * @av: argumnet vector
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int Number2, (*Pointer_Obtener_Operador)(int Number1, int Number2);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	Pointer_Obtener_Operador = get_op_func(&*argv[2]);
+	if (!Pointer_Obtener_Operador)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	Number2 = atoi(*(argv + 3));
+
+	if (!Number2 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", Pointer_Obtener_Operador(atoi(*(argv + 1)), Number2));
+	return (0);
+}
