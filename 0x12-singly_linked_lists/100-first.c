fb3a3e7faab36b110ecd56510ab93ab769721c4f
JessicaJulieth <3497@holbertonschool.com>
2021-08-01 16:57:39 -0500

    Correction with betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
-# include "lists.h"
-
+#include "lists.h"
 /**
- * premain - Prints before the main
- * premain - Prints before the main
+ * premain - new function.
+ *
+ * Return: Always 0.
  */
-
-int premain(void) __attribute__ ((constructor));
+int premain(void) __attribute__((constructor));
 int premain(void)
 {
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
-return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
 }
