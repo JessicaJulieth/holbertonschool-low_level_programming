ef8d77539f1ee3fbc2af393b96e39365db588de0
JessicaJulieth <3497@holbertonschool.com>
2021-08-01 13:16:56 -0500

    Add ident

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,7 +4,7 @@
  * add_node - Adds a new node at the beginning of a list_t list
  * @head:Pointer
  * @str: String of new node
- * 
+ *
  * Return:The address of the new element, or NULL if it failed
  */
 
@@ -14,20 +14,18 @@ list_t *add_node(list_t **head, const char *str)
 	int x = 0;
 
 	n = malloc(sizeof(list_t));
-	
-	if (n == NULL)
-    {
-    return (NULL); 
-    }
+if (n == NULL)
+{
+return (NULL);
+}
 
 	while (str[x] != '\0')
 		x++;
 	n->str = strdup(str);
-	
 	n->len = x;
 	n->next = (*head);
 	(*head) = n;
-    {
-    return (n);   
-    }
+{
+return (n);
+}
 }
