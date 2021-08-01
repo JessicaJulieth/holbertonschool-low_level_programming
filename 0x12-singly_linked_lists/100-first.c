02461f56218815bf7824113c2cc4295b741a73fc
JessicaJulieth <3497@holbertonschool.com>
2021-08-01 15:15:45 -0500

    Print before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+# include "lists.h"
+
+/**
+ * premain - Prints before the main
+ * premain - Prints before the main
+ */
+
+int premain(void) __attribute__ ((constructor));
+int premain(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+return (0);
+}
