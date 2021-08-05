a0bf07b780884f66451c27651cac647ba58807da
JessicaJulieth <3497@holbertonschool.com>
2021-08-04 22:48:19 -0500

    Prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,18 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t i;
-for (i = 0; h != NULL; i++)
-{
-if (h->str == NULL)
-{
-printf("[0] (nil");
-}
-else
-{
-printf("[%i] %s, h->len, h->str");
-}
-h = h->next;
-}
-return (i);
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
