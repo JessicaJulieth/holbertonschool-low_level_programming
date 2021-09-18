6fc91920140dee046d2aeb0ec89ade9d58ec0ebf
JessicaJulieth <3497@holbertonschool.com>
2021-09-17 23:37:07 -0500

    Add node to the end of the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,48 +1,51 @@
 #include "lists.h"
-
+/**
+ * add_dnodeint_end - Add node to the end of the list.
+ * @head: Pointer to the address to the node
+ * @n: Pointer to the list
+ *
+ * Return: Node of the list
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    if (head == NULL) {
-        return NULL;
-    }
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		dlistint_t *new = NULL;
 
-    if (*head == NULL) {
-        dlistint_t *new = NULL;
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+		{return (NULL);
+		}
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		dlistint_t *temp = *head;
+		dlistint_t *new = NULL;
 
-        new = malloc(sizeof(dlistint_t));
-
-        if (new == NULL) {
-            return NULL;
-        }
-        new->n = n;
-        new->prev = NULL;
-        new->next = NULL;
-
-        *head = new;
-
-        return new;
-    } else {
-    /* Recorrer hasta llegar a NULL */
-        dlistint_t *temp = *head;
-        dlistint_t *new = NULL;
-
-        while(temp != NULL) {
-
-            if (temp->next == NULL) {
-                break;
-            }
-            temp = temp->next; 
-        }
-        new = malloc(sizeof(dlistint_t));
-
-        if (new == NULL) {
-            return NULL;
-        }
-        new->n = n;
-        new->prev = temp;
-        new->next = NULL;
-        temp->next = new;
-
-        return new;
-    }
+		while (temp != NULL)
+	{
+			if (temp->next == NULL)
+			{ break;
+			}
+			temp = temp->next;
+	}
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+		{return (NULL);
+		}
+		new->n = n;
+		new->prev = temp;
+		new->next = NULL;
+		temp->next = new;
+		return (new);
+	}
 }
