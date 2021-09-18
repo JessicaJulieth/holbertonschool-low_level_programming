2186b6b4c0ce8aea11097e6522703899c03a885d
JessicaJulieth <3497@holbertonschool.com>
2021-09-17 23:08:25 -0500

    Print length of the list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,19 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - Print len of list.
+ * @h: Pointer to the list
+ *
+ * Return: len of the list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t counter = 0;
 
-    size_t counter = 0; 
-
-    while(h != NULL) 
-    {
-        counter++;
-        h = h->next; 
-    }
-
-    return counter;
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
 }
