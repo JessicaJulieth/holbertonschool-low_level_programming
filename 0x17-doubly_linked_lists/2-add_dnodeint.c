f3a479a6dd4f259ff646d85f835f41efc97ae82b
JessicaJulieth <3497@holbertonschool.com>
2021-09-17 23:18:11 -0500

    Add node to the list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,41 +1,51 @@
 #include "lists.h"
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n) 
+/**
+ * add_dnodeint - Add node to the list.
+ * @head: Pointer to the address to the node
+ * @n: Pointer to the list
+ *
+ * Return: Node of the list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	if (head != NULL && *head != NULL)
+	{
 
-    if (head != NULL && *head != NULL) {
+		dlistint_t *new = NULL;
 
-        dlistint_t *new = NULL;
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+		{
+			return (NULL);
+		}
+		new->n = n;
+		new->prev = NULL;
+		new->next = *head;
 
-        new = malloc(sizeof(dlistint_t));
-        if (new == NULL) {
-            return NULL;
-        }
-        new->n = n;
-        new->prev = NULL;
-        new->next = *head;
+		*head = new;
 
-        *head = new;
+		return (new);
 
-        return new;
+	}
+	else if (head != NULL && *head == NULL)
+	{
+		dlistint_t *new = NULL;
 
-    }
-    else if (head != NULL && *head == NULL) {
-        dlistint_t *new = NULL;
+		new = malloc(sizeof(dlistint_t));
 
-        new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+		{
+			return (NULL);
+		}
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
 
-        if (new == NULL) {
-            return NULL;
-        }
-        new->n = n;
-        new->prev = NULL;
-        new->next = NULL;
+		*head = new;
 
-        *head = new;
+		return (new);
+	}
 
-        return new;
-    }
-
-    return NULL;
+	return (NULL);
 }
