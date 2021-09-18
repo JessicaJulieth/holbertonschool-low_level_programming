4a90a851f7966512ad3fbe260bdd7e38256f4f42
JessicaJulieth <3497@holbertonschool.com>
2021-09-17 23:00:20 -0500

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,22 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Print list.
+ * @h: Pointer to the list
+ *
+ * Return: numbers of the list
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t counter = 0;
 
-    size_t counter = 0; 
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+		counter++;
+		h = h->next;
+	}
 
-    while(h != NULL) 
-    {
-        printf("%d\n", h->n);
-        counter++;
-        h = h->next; 
-    }
-
-    return counter;
+	return (counter);
 }
