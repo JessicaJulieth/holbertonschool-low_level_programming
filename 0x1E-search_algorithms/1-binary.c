6554eff23f63dd1fc08a8546e1faf5c03ca31826
JessicaJulieth <3497@holbertonschool.com>
2022-01-25 10:57:35 -0500

    searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search- searches for a value in an array of integers
+ * binary_search - searches for a value in an array of integers
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in array
  * @value: Value to search for
@@ -11,7 +11,7 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t x = 0, y = 0, z= 0, i = size - 1;
+	size_t x = 0, y = 0, z = 0, i = size - 1;
 
 	if (!array)
 		return (-1);
